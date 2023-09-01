e9a770435e88d1dffff6364dc3c49941ea8bcf3d
Dheemanth <Deemanthdb18@gmail.com>
2023-09-01 11:33:28 +0530

    division added

---
+++ arith.py
@@ -0,0 +1,23 @@
+def add (n1,n2):
+    return(n1+n2)
+
+def mul(n1,n2):
+    return(n1*n2)
+
+def sub(n1,n2):
+    return(n1-n2)
+
+def div(n1,n2):
+    return(n1/n2)
+
+
+
+
+a=int(input("Enter num1 :"))
+b=int(input("Enter num2 :"))
+print("Ans=",add(a,b))
+
+print("Ans=",mul(a,b))
+print("Ans=",sub(a,b))
+print("Ans=",div(a,b))
+
